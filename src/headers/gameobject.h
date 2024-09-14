eefbf0dae61de0084e95f7673f55d4ea4032970c
ivan <ivan.shvydchenko2@mail.dcu.ie>
2024-09-14 21:24:03 +0100

    added ImGUI library for handling UI

---
+++ src/headers/gameobject.h
@@ -0,0 +1,36 @@
+#pragma once
+
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+#include <string>
+
+class GameObject{
+
+// public func
+public:
+    GameObject(SDL_Renderer* renderer, int width, int height, int x, int y, int r, int g, int b, int a);
+    GameObject(SDL_Renderer* renderer, std::string filename, int width, int height, int x, int y);
+    ~GameObject();
+
+    // renderer
+    void Render();
+
+    // Texture
+    SDL_Texture* Texture(const std::string filename);
+    
+    // inline function which returns id
+    inline int GetID() const { return _id; }
+
+// private members
+private:
+    static int _current_id;
+    int _id;
+    int _width;
+    int _height;
+    int _x, _y;
+    int _r, _g, _b;
+    int _a = 255;
+    SDL_Rect _dest_rect;
+    SDL_Renderer* _objRenderer;
+    SDL_Texture* _objTexture = nullptr;
+};
--- src/scripts/Main.cpp
+++ src/scripts/Main.cpp
@@ -2,12 +2,22 @@
 #include <window.h>
 #include <gameobject.h>
 #include <iostream>
+#include "imgui.h"
+#include "imgui_impl_sdl2.h"
+#include "imgui_impl_sdlrenderer2.h"
 
 int main(int argc, char **argv){
     
     // created a window object
     Window window("GAME ENGINE", 800, 640);
     
+    // initialize and setup for ImGUI
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+    ImGuiIO& io = ImGui::GetIO();
+    ImGui_ImplSDL2_InitForSDLRenderer(Window::window, Window::renderer);
+    ImGui_ImplSDLRenderer2_Init(Window::renderer);
+
     // creates gameObject types 
     GameObject player(Window::renderer, 100, 100, 500, 200, 107, 55, 200, 255);
     GameObject player2(Window::renderer, 100, 100, 200, 200, 55, 55, 200, 255);
@@ -17,7 +27,6 @@ int main(int argc, char **argv){
     GameObject player4(Window::renderer, "C:\\Users\\shvdi\\Pictures\\gyro_zepelli.jpg", 500, 500, 100, 100);
     
 
-    
     // creates a SDL event pointer
     SDL_Event event;
     
@@ -36,16 +45,42 @@ int main(int argc, char **argv){
         // needs the be called to register events like key presses
         if(SDL_PollEvent(&event)){
             window.pollEvents(event);
+
+            // All these start the ImGUI frame
+            ImGui_ImplSDLRenderer2_NewFrame();
+            ImGui_ImplSDL2_NewFrame();
+            ImGui::NewFrame();
+
+            // Creates new ImGUI window for testing atm
+            ImGui::Begin("Tester");
+            ImGui::SetWindowSize(ImVec2(100,100));
+            ImGui::End();
+
+            // Wraps up all ImGUI elements and compiles everything into a ImDrawData struct
+            ImGui::Render();
+
             // renders the objects to the screen without this wont display
             // NOTE: THE ORDER IN WHICH YOU RENDER CAN BE SEEN AS "LAYERS"
             player4.Render();
             player.Render();
             player2.Render();
             player3.Render();
+            
+            // allows for interaction with the ImGui window created
+            ImGui_ImplSDL2_ProcessEvent(&event);
+            
+            // Draws IMGUI to the renderer to be ready to be presented on window by getting ImDrawData struct
+            ImGui_ImplSDLRenderer2_RenderDrawData(ImGui::GetDrawData(), Window::renderer);
+
             // displays whole window with all the rendering
             window.createDisplay();
         }
     }
+
+    // Destroys ImGUI elements
+    ImGui_ImplSDLRenderer2_Shutdown();
+    ImGui_ImplSDL2_Shutdown();
+    ImGui::DestroyContext();
     
     return 0;
 
